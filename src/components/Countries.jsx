93cafe6591f941fe8f5cde8f9e2e652f4d8020e1
gaellebega <gaellebega@gmail.com>
2024-04-03 10:29:21 +0200

    secondcommit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import { CountriesSection } from '../styled-components/GeneralComponents'
-
-const Countries = () => {
+import React, {useState, useEffect} from 'react'
+function Countries() {
+   const[countryData, setCountryData] =  useState([]);
+   const [currentPage] = useState(1);
+   const countriesPerPage = 250;
+   useEffect(() =>{
+    fetch('https://restcountries.com/v3/all')
+    .then(response => response.json())
+    .then(data => setCountryData(data));
+   }, []);
+   const indexOfLastCountry = currentPage * countriesPerPage;
+   const indexOfFirstCountry = indexOfLastCountry - countriesPerPage;
+   const currentCountries = countryData.slice(indexOfFirstCountry, indexOfLastCountry);
   return (
-    <CountriesSection>
-      {/* Top part  */}
-      <div id='top-section'>
-        <div>
-          <h3>View Countries</h3>
+     <section>
+      <div className='flex justify-around mb-10 mt-10'>
+        <div >
+          <h2>View Countries</h2>
           <p>Page 1 of 5</p>
         </div>
         <select>
-          <option value="">Select region</option>
-        </select>
-      </div>
-
-      {/* List of countries */}
-      <div id='countries'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, iure error! Nulla, soluta sint. Repellendus, id, necessitatibus voluptatum recusandae enim excepturi quisquam labore nam quam ad ea in explicabo aspernatur!
-      </div>
-
-      {/* Pagination  */}
-      <div id='pagination'>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-      </div>
-    </CountriesSection>
-  )
+          <option value="Select Continent">Select Continent</option>
+            </select>
+            </div>
+            <div className=''>
+            <ul className="flex flex-wrap gap-10 justify-center items-center">
+           {currentCountries.map(country => (
+             <div className="items-center md:w-4/12 lg:w-2/12 justify-center" key={country.name.common}>
+              <img src={`https://flagcdn.com/${country.cca2.toLowerCase()}.svg`} alt={country.name.common} className="w-[250px] items-center h-[120px]"/>
+              <strong>{country.name.common}</strong>
+             <p><strong>Capital</strong>: {country.capital}</p>
+             <p><strong>Population</strong>: {country.population}</p>
+               <p><strong>Region</strong>: {country.region}</p>
+            </div>
+           ))}
+         </ul>
+    </div>
+    </section>
+   );
 }
-
-export default Countries
+export default Countries;
